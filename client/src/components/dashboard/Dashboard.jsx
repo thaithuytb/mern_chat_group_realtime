a5ae61035e3b98853216bd1bb3bce3be92e4bdca
thaithuytb <ngoquangthai29112001@gmail.com>
2021-07-09 10:36:54 +0700

    finish show online myfriends online

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { io } from "socket.io-client";
+import { REACT_APP } from "./../../config/constants";
 import "./dashboard.css";
-
 const Dashboard = ({ user }) => {
+  const userOnline = useRef();
+  useEffect(() =>{
+    userOnline.current = io(REACT_APP, { transports : ["websocket"]});
+    userOnline.current.emit("send-user-info" , user._id);
+  },[user]);
+
   return (
     <div className="dashboard">
       <h1>WELCOME TO MY APP</h1>
